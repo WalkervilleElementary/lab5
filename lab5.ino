f52f2b7ac31242c13ce5ee108b46c41fd187e830
Lise Savard <lise@savard.cc>
2017-06-15 17:13:32 -0700

    Fix heading directions, add PWM lookup table

--- lab5.ino
+++ lab5.ino
@@ -1,8 +1,8 @@
 #include <phys253.h>          
 #include <LiquidCrystal.h>
 
-#define RIGHT_MOTOR 0
-#define LEFT_MOTOR 1
+#define LEFT_MOTOR 0
+#define RIGHT_MOTOR 1
 
 float gain;
 float kp;
@@ -17,30 +17,37 @@ unsigned long lastTime;
 float heading;
 int motorSpeed;
 
+unsigned int PWM_MAP[256];
+
 unsigned int get_error() {
   return 0;
 }
 
 /**
- * Change car's direction. Positive heading = turn right.
- * Negative heading = turn left. Heading of 0 = go straight.
+ * Change car's direction. Positive heading = turn left.
+ * Negative heading = turn right. Heading of 0 = go straight.
+ * Requires: -1 <= heading <= -1
  */
-void updateHeading(int heading) {
-  // requirements: -1 < heading < 1  
-  int rightSpeed = motorSpeed * (1 + heading);
-  int leftSpeed = motorSpeed * (1 - heading);
+void updateHeading() {
+  int rightSpeed = (int) (motorSpeed * (1 + heading));
+  int leftSpeed = (int) (motorSpeed * (1 - heading));
   
-  motor.speed(RIGHT_MOTOR, (rightSpeed > 255) ? 255 : rightSpeed);
-  motor.speed(LEFT_MOTOR, (leftSpeed < 255 ) ? 255 : leftSpeed);
+  motor.speed(RIGHT_MOTOR, (rightSpeed > 255) ? 255 : PWM_MAP[rightSpeed]);
+  motor.speed(LEFT_MOTOR, (leftSpeed > 255 ) ? 255 : PWM_MAP[leftSpeed]);
 }
 
 void setup()
 {  
   #include <phys253setup.txt>
 
+  // populate PWM table
+  for (int i = 0; i < 256; i++) {
+    PWM_MAP[i] = (int) (50 + i * 0.804);
+  }
+
   // initial values
   heading = 0;
-  motorSpeed = 60;
+  motorSpeed = 100;
   
   gain = 0.01;
   kp = 1.0;
@@ -58,28 +65,28 @@ void setup()
 void loop()
 {
   unsigned long now = millis();
-  float dt = (now - lastTime) * 0.001;
-
-  error = get_error();
-
-  float dO = gain * ((error - lastError) + ki * dt * error + 
-            kd * (error - 2 * lastError + lastLastError) / dt);
-
-  // update errors
-  lastLastError = lastError;
-  lastError = error;
-  lastTime = now;
-
-  // calculate heading, make sure constrained to [-1, 1]
-  heading += dO;
-  if (heading > 1)
-    heading = 1;
-  else if (heading < -1)
-    heading = -1;
-  updateHeading(heading);
-
-  delay(10); // make dt more predictable
-
-  // currently our wheels can't go backwards. Is this a problem?
+  // don't do this more than every 10 milliseconds
+  // keep dt more consistent
+  if (now >= lastTime + 10) {
+    float dt = (now - lastTime) * 0.001;
+  
+    error = get_error();
+  
+    float dO = gain * ((error - lastError) + ki * dt * error + 
+              kd * (error - 2 * lastError + lastLastError) / dt);
+  
+    // update errors
+    lastLastError = lastError;
+    lastError = error;
+    lastTime = now;
+  
+    // calculate heading, make sure constrained to [-1, 1]
+    heading += dO;
+    if (heading > 1)
+      heading = 1;
+    else if (heading < -1)
+      heading = -1;
+    updateHeading();
+  }
 }
 
